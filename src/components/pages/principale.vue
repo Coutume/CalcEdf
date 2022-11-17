65334bcd4215b40d21710dc3075deee8e05b43fa
Florian JEAN <florian.jean840@gmail.com>
2022-11-17 11:20:33 +0100

    Ajout navigation

--- src/components/pages/principale.vue
+++ src/components/pages/principale.vue
@@ -6,10 +6,30 @@
         dark
         app
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="menuPrincipal = !menuPrincipal"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Calculateur EDF > {{ $route.meta.nom }}</v-toolbar-title>
     </v-app-bar>
+    <v-navigation-drawer
+        v-model="menuPrincipal"
+        absolute
+        bottom
+        temporary
+    >
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item-group
+            v-model="listeMenuLien"
+            active-class="green--text text--accent-4"
+        >
+          <v-list-item :to="route.path" v-for="route in $router.options.routes">
+            <v-list-item-title>{{ route.meta.nom }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -18,7 +38,13 @@
 
 <script>
 export default {
-  name: "principale"
+  name: "principale",
+  data() {
+    return {
+      menuPrincipal: false,
+      listeMenuLien: null,
+    }
+  }
 }
 </script>
 
