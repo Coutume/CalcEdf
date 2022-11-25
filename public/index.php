8abe2fd9597afdcf726e40f1de13cc5f755cbc3a
Florian JEAN <florian.jean840@gmail.com>
2022-11-25 22:38:21 +0100

    Redirect all requests from /api/[...] to slim front controller (public/index.php)

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -6,9 +7,15 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+$app->setBasePath('/api');
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("shigoto wo hajimemashou !");
+    return $response;
+});
+
+$app->get('/test', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Premier webservice");
     return $response;
 });
 
